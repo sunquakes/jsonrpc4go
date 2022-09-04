b0cb7d6c53aeb1a58b738603d0ebf883857f0614
Robert <sunquakes@outlook.com>
2022-09-04 09:43:32 +0800

    Add tcp client connect pool.

--- client/pool.go
+++ client/pool.go
@@ -7,39 +7,60 @@ import (
 )
 
 type Option struct {
-	MaxIdle   int
-	MaxActive int
 	MinIdle   int
+	MaxActive int
+	MaxIdle   int
 }
 
 type Pool struct {
-	Lock      sync.Mutex
-	Config    Option
-	ConnTotal int
-	Conns     chan net.Conn
+	Address     string
+	Lock        sync.Mutex
+	Option      Option
+	ActiveTotal int
+	Conns       chan net.Conn
 }
 
 func NewPool(ip string, port string, option Option) *Pool {
 	ch := make(chan net.Conn, 10)
 	var addr = fmt.Sprintf("%s:%s", ip, port)
-	for i := 0; i < option.MinIdle; i++ {
-		conn, err := net.Dial("tcp", addr)
-		if err == nil {
-			ch <- conn
-		}
-	}
-	return &Pool{
+	pool := &Pool{
+		addr,
 		sync.Mutex{},
 		option,
 		0,
 		ch,
 	}
+	for i := 0; i < option.MinIdle; i++ {
+		err := pool.NewConn()
+		if err != nil {
+			fmt.Errorf("Can not connect %s", addr)
+		}
+	}
+	return pool
 }
 
 func (p *Pool) Borrow() net.Conn {
+	p.Lock.Lock()
+	defer p.Lock.Unlock()
+	if p.ActiveTotal >= p.Option.MaxActive-p.Option.MinIdle {
+		return <-p.Conns
+	}
 	return <-p.Conns
 }
 
 func (p Pool) Release(conn net.Conn) {
+	p.Lock.Lock()
+	defer p.Lock.Unlock()
 	p.Conns <- conn
+}
+
+func (p Pool) NewConn() error {
+	p.Lock.Lock()
+	defer p.Lock.Unlock()
+	conn, err := net.Dial("tcp", p.Address)
+	if err == nil {
+		p.ActiveTotal++
+		p.Conns <- conn
+	}
+	return err
 }
